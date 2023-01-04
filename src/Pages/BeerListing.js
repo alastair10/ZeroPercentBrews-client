84d7d8ef6084439621c416de065f46092bf3c8df
adonl <adnlwly@gmail.com>
2023-01-04 18:38:12 +0000

    add initial beerlisting page styling

--- src/Pages/BeerListing.js
+++ src/Pages/BeerListing.js
@@ -1,14 +1,17 @@
 import BeerCardExpanded from "../Components/Beer/BeerCardExpanded";
 import BeerInfo from "../Components/Beer/BeerInfo";
 import SocialProof from "../Components/SocialProof/SocialProof";
+import "./BeerListing.css"
 
 const BeerListing = () => {
 
   return (
     <>
-      <BeerCardExpanded />
-      <BeerInfo />
-      <SocialProof />
+      <div className='beer__listing__container'>
+        <BeerCardExpanded />
+        <BeerInfo />
+        <SocialProof />
+      </div>
     </>
   );
 
