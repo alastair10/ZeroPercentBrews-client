08e41563b38e916b8da71ae9c0146addf46b7c67
adonl <adnlwly@gmail.com>
2023-01-04 19:40:22 +0000

    update beercard styling

--- src/Components/Beer/BeerCardExpanded.js
+++ src/Components/Beer/BeerCardExpanded.js
@@ -1,22 +1,31 @@
 import star from '../../images/star.svg';
 import beerImage from '../../images/beer_placeholder.png';
+import './BeerCardExpanded.css';
 
 const BeerCardExpanded = () => {
   return (
     <>
       <h1 className='beer__title'>Moretti Zero</h1>
-      <img className='beer__image' src={beerImage} />
-      <div className='social__proof'>
-        <div className='rating__info'>
-          <div className='rating__score'>4.5</div>
-          <img src={star} className='rating__star' alt='star__icon'/>
+      <div className='beer__card__container'>
+
+          <img className='beer__image' src={beerImage} />
+
+        <div className='beer__basic__info'>
+
+          <div className='social__proof'>
+
+            <div className='rating'>
+              <div className='rating__score'>4.5</div>
+              <img src={star} className='rating__star' alt='star__icon'/>
+            </div>
+
+            <div className='reviews__info'>3 Reviews</div>
+          </div>
+   
+          <div className='basic__info__item'><span className='attribute'>From:</span> Italy</div>
+          <div className='basic__info__item'><span className='attribute'>Calories:</span> 66 kcal</div>
+          <div className='basic__info__desc'>A refreshing and light taste. Great with pasta, pizza or on its own.</div>
         </div>
-        <div className='reviews__info'>3 Reviews</div>
-      </div>
-      <div className='basic__info'>
-        <div className='basic__info__item'>From: Italy</div>
-        <div className='basic__info__item'>Calories: 66 kcal</div>
-        <div className='basic__info__desc'>A refreshing and light taste. Great with pasta, pizza or on its own.</div>
       </div>
     </>
   );
--- src/Components/Beer/BeerInfo.js
+++ src/Components/Beer/BeerInfo.js
@@ -3,11 +3,11 @@ const BeerInfo = () => {
   return (
     <>
       <h2 className='info__header'>About Moretti Zero</h2>
-      <div className='info__item'>Brand: Birra Moretti</div>
-      <div className='info__item'>ABV: 0.0%</div>
-      <div className='info__item'>Type: Lager</div>
-      <div className='info__item'>Ingredients: Water, Barley Malt, Hop, Hop Extract, Natural Flavouring</div>
-      <div className='info__item'>Volume: 330ml</div>
+      <div className='info__item'><span className='attribute'>Brand:</span> Birra Moretti</div>
+      <div className='info__item'><span className='attribute'>ABV:</span> 0.0%</div>
+      <div className='info__item'><span className='attribute'>Type:</span> Lager</div>
+      <div className='info__item'><span className='attribute'>Ingredients:</span> Water, Barley Malt, Hop, Hop Extract, Natural Flavouring</div>
+      <div className='info__item'><span className='attribute'>Volume:</span> 330ml</div>
     </>
   )
 
