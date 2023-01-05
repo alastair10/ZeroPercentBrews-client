d3aa012ea885fac989cc55663cb61029eb3dd92f
Joseph Lander <josephclander@gmail.com>
2023-01-05 16:34:01 +0000

    Add fetch request

--- src/Components/Beer/BeerContainer.js
+++ src/Components/Beer/BeerContainer.js
@@ -1,11 +1,11 @@
 import styles from './BeerContainer.module.css';
 import BeerCard from './BeerCard';
-import data from '../../tests/mockData';
+// import data from '../../tests/mockData';
 
-const BeerContainer = () => {
+const BeerContainer = ({ beerData }) => {
   return (
     <div className={styles.beer_container}>
-      {data.map((beer) => {
+      {beerData.map((beer) => {
         return <BeerCard key={beer._id} beerInfo={beer} />;
       })}
     </div>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,22 @@
 import BeerContainer from '../Components/Beer/BeerContainer';
 import Hero from '../Components/Core/Hero';
+import { useState, useEffect } from 'react';
 
 const Home = () => {
+  const [beerData, setBeerData] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://zero-percent-brews-api.onrender.com/api/beers/`)
+      .then((response) => response.json())
+      .then((data) => {
+        setBeerData(data);
+      });
+  }, []);
+
   return (
     <>
       <Hero />
-      <BeerContainer />
+      <BeerContainer beerData={beerData} />
     </>
   );
 };
