69136d5885bd62ec8ecd1cfb4b95a7735abf5f26
Alastair <alastair.chau@gmail.com>
2023-01-05 12:54:28 +0000

    cors enabled fetch working

--- src/Pages/BeerListing.js
+++ src/Pages/BeerListing.js
@@ -1,20 +1,35 @@
 import BeerCardExpanded from "../Components/Beer/BeerCardExpanded";
 import BeerInfo from "../Components/Beer/BeerInfo";
 import SocialProof from "../Components/SocialProof/SocialProof";
-import "./BeerListing.css"
+import "./BeerListing.css";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const BeerListing = () => {
+  const { beer_id } = useParams();
+  
+  const [beerData, setBeerData] = useState();
+
+  console.log(beer_id)
+  useEffect(() => {
+    fetch(`http://localhost:4000/api/beers`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+      });
+  }, []);
+
+  // console.log(beerData);
 
   return (
     <>
-      <div className='beer__listing__container'>
+      <div className="beer__listing__container">
         <BeerCardExpanded />
         <BeerInfo />
         <SocialProof />
       </div>
     </>
   );
-
 };
 
 export default BeerListing;
