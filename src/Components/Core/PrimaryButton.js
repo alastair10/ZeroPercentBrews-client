beb125fa0590ca9f0000dc48e5e803dba7397325
Tom <tomfyfe85@gmail.com>
2023-01-05 15:15:42 +0000

    primary button

---
+++ src/Components/Core/PrimaryButton.js
@@ -0,0 +1,11 @@
+import "./PrimaryButton.css";
+
+const PrimeButton = () => {
+  return (
+    <div className="link">
+      <a href="../Components/Beer/BeerCardExpanded">More Info</a>
+    </div>
+  );
+};
+
+export default PrimeButton;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import Hero from "../Components/Core/Hero";
+import PrimeButton from "../Components/Core/PrimaryButton";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <div>Placeholder</div>
+      <PrimeButton />
     </>
   );
 };
