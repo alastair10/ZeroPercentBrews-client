f7d01eda96cc004fc1fb90d2883f21230082c862
Tom <tomfyfe85@gmail.com>
2023-01-05 09:55:28 +0000

    navbar mvp

--- src/Components/Core/Nav.js
+++ src/Components/Core/Nav.js
@@ -1,27 +1,10 @@
-
+import logo from "./logo.svg";
 
 const Navbar = () => {
   return (
-    <div>
-     
-      <nav className="navbar">
-      <img src="./download.jpg" alt="svg logo" />
-       <div className="links">
-        <a href="/login" style={{
-          color: "#1E6A9C",
-          backgroundColor: "white",
-          borderRadius: '4px',
-          
-        }}>Log in</a>
-       
-        <a href="/signup" style={{
-          color: "white",
-          backgroundColor: "#1E6A9C",
-          borderRadius: '4px'
-        }}>Sign Up</a>
-        </div>
-      </nav>
-    </div>
+    <nav className="navbar">
+      <img src={logo} alt="svg logo" />
+    </nav>
   );
 };
 
