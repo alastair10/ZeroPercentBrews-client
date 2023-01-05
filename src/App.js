8f12e2837f511cd79b8569b70bf52633ef478169
Tom <tomfyfe85@gmail.com>
2023-01-05 11:19:21 +0000

    hero needs styling/(Actualy)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import BeerListing from './Pages/BeerListing';
 import Home from './Pages/Home'
 import { Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Core/Nav';
+import Navbar from './Components/Core/Nav'
+import Hero from './Components/Core/Hero'
 
 function App() {
   return (
     <>
     <Navbar />
+     <Hero />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/beer/:id" element={<BeerListing />} />
---
+++ src/Components/Core/Hero.js
@@ -0,0 +1,16 @@
+
+
+const hero = () => {
+  return ( 
+   
+      <div>
+      
+       <h3>
+       <p className="heroTop">Actually Not</p>
+       <p className="heroBottom">That Bad</p>
+       </h3>
+    
+       </div>
+     )}
+ 
+export default hero;
