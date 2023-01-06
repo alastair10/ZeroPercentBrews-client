fb4ee211809c43a7491834546df4113be72e96e8
Alastair <alastair.chau@gmail.com>
2023-01-06 15:23:13 +0000

    Add Register page functions handleSubmit and handleChange

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,31 +1,49 @@
+import { useState } from 'react'; 
+
 const register = () => {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(inputs);
+  }
+  
   return ( 
-    <form className="register-form">
-      <label>Username</label>
+    <form onSubmit={handleSubmit}>
+      <label>Username
       <input
           placeholder='e.g. BeerMaster4000'
-          id='username'
           type='text'
-          value={username}
-          onChange={}
+          name='username'
+          value={inputs.username}
+          onChange={handleChange}
         />
-      <label>Email</label>
+      </label>
+      <label>Email
       <input
           placeholder='e.g. name@address.com'
-          id='email'
-          type='text'
-          value={email}
-          onChange={}
+          type='email'
+          name='email'
+          value={inputs.email}
+          onChange={handleChange}
         />
-      <label>Password</label>
+      </label>
+      <label>Password
       <input
           placeholder='make a secure password'
-          id='password'
           type='password'
-          value={password}
-          onChange={}
+          name='password'
+          value={inputs.password}
+          onChange={handleChange}
         />
-      <button type="submit" onClick={}>Sign Up</button>
+        </label>
+      <input type="Sign Up" />
     </form>
   );
 }
