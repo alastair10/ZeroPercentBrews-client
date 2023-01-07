01b1b15ce95dd58ac780c2ff1c59ef786768c695
Joseph Lander <josephclander@gmail.com>
2023-01-07 18:17:50 +0000

    Add minor adjustments

--- src/Components/Core/Nav.js
+++ src/Components/Core/Nav.js
@@ -1,22 +1,19 @@
-import logo from "../../images/primary-logo.svg";
-import { Link } from "react-router-dom";
-import "./Nav.css";
-import ButtonTertiary from "./ButtonTertiary";
-import ButtonSecondary from "./ButtonSecondary";
+import logo from '../../images/primary-logo.svg';
+import { Link } from 'react-router-dom';
+import './Nav.css';
+import ButtonTertiary from './ButtonTertiary';
+import ButtonSecondary from './ButtonSecondary';
 
 const Navbar = () => {
   return (
-    <nav className="nav">
-      <div className="nav__container">
-        <Link className="nav__link--logo" to="/">
-          <img className="nav__logo" src={logo} alt="svg logo" />
+    <nav className='nav'>
+      <div className='nav__container'>
+        <Link className='nav__link--logo' to='/'>
+          <img className='nav__logo' src={logo} alt='svg logo' />
         </Link>
-
-        <div className="signin">
-          <ButtonTertiary path={"/login"} text={"Log In"} />
-        </div>
-        <div className="signup">
-          <ButtonSecondary path={"/register"} text={"Sign Up"} />
+        <div className='nav__buttons'>
+          <ButtonTertiary path={'/login'} text={'Log In'} />
+          <ButtonSecondary path={'/register'} text={'Sign Up'} />
         </div>
       </div>
     </nav>
