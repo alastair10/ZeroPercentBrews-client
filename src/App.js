c8a066f487708de3a080e3ccdf2d2275441021a0
Alastair <alastair.chau@gmail.com>
2023-01-07 15:11:27 +0000

    BeerReview route added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import BeerListing from "./Pages/BeerListing";
-import Home from "./Pages/Home";
 import { Routes, Route } from "react-router-dom";
+import BeerReview from './Pages/BeerReview';
+import BeerListing from "./Pages/BeerListing";
 import Navbar from "./Components/Core/Nav";
 import Footer from "./Components/Core/Footer";
 import Register from "./Pages/Register";
 import Login from "./Pages/Login";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
+        <Route path="/beer/:id/review" element={<BeerReview />} />
+        <Route path="/beer/:id" element={<BeerListing />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
-        <Route path="/beer/:id" element={<BeerListing />} />
       </Routes>
 
       <Footer />
