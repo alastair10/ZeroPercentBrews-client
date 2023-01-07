74d8f8572d15ef19b06dac19b4d37562790ebd2b
Joseph Lander <josephclander@gmail.com>
2023-01-07 07:45:14 +0000

    Add lazy loading tag to images

--- src/Components/Beer/BeerCard.js
+++ src/Components/Beer/BeerCard.js
@@ -6,7 +6,12 @@ const BeerCard = ({ beerInfo }) => {
   return (
     <div className={styles.beer}>
       <div className={styles.beer__card__container}>
-        <img className={styles.beer__image} src={beerInfo.image} alt='' />
+        <img
+          className={styles.beer__image}
+          src={beerInfo.image}
+          alt=''
+          loading='lazy'
+        />
         <div className={styles.beer__basic__info}>
           <h3 className={styles.beer__title}>{beerInfo.title}</h3>
           <div className={styles.social__proof}>
@@ -18,6 +23,7 @@ const BeerCard = ({ beerInfo }) => {
                 src={star}
                 className={styles.rating__star}
                 alt='star__icon'
+                loading='lazy'
               />
             </div>
             <a className={styles.reviews__info} href='#reviews'>
