eeb63c72a503baf98e78b41477cefba05004a435
Joseph Lander <josephclander@gmail.com>
2023-01-07 22:41:41 +0000

    Add responsiveness to hero image and text

--- src/Components/Core/Hero.js
+++ src/Components/Core/Hero.js
@@ -1,11 +1,21 @@
-import "./Hero.css";
+import './Hero.css';
 
 const hero = () => {
   return (
-    <div className="hero">
-      <p className="hero__top">
-        <span className="hero__span">Actually</span> Not That Bad
-      </p>
+    <div className='hero'>
+      <div className='hero__container'>
+        <p className='hero__tagline'>
+          <span className='hero__span'>Actually</span> Not That Bad
+        </p>
+        <div className="hero__message">
+          <p className='hero__message_1'>
+            Find the best tasting,
+          </p>
+          <p className='hero__message_2'>
+            hangover free beers.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
