f9d1d5c39a1fc8b3d28e08ac74e45c95d1303311
Tom <tomfyfe85@gmail.com>
2023-01-07 15:17:42 +0000

    fixed routing bug

--- src/Components/Core/Nav.js
+++ src/Components/Core/Nav.js
@@ -1,26 +1,24 @@
-import logo from '../../images/primary-logo.svg';
-import { Link } from 'react-router-dom';
-import './Nav.css';
-import ButtonTertiary from './ButtonTertiary';
-import ButtonSecondary from './ButtonSecondary';
-
+import logo from "../../images/primary-logo.svg";
+import { Link } from "react-router-dom";
+import "./Nav.css";
+import ButtonTertiary from "./ButtonTertiary";
+import ButtonSecondary from "./ButtonSecondary";
 
 const Navbar = () => {
   return (
-    <nav className='nav'>
-      <div className='nav__container'>
-        <Link className='nav__link--logo' to='/'>
-          <img className='nav__logo' src={logo} alt='svg logo' />
+    <nav className="nav">
+      <div className="nav__container">
+        <Link className="nav__link--logo" to="/">
+          <img className="nav__logo" src={logo} alt="svg logo" />
         </Link>
-        {/* <div className='nav__buttons'> */}
-          <div className='signin'>
-          <ButtonTertiary path={`/login}`} text={'Log In'} />
-          </div>
-          <div className='signup'>
-            <ButtonSecondary path={`/register}`} text={'Sign Up'} />
-          </div>
+
+        <div className="signin">
+          <ButtonTertiary path={"/login"} text={"Log In"} />
         </div>
-      {/* </div> */}
+        <div className="signup">
+          <ButtonSecondary path={"/register"} text={"Sign Up"} />
+        </div>
+      </div>
     </nav>
   );
 };
