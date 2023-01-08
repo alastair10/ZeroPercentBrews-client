a1bbe055141ab5e32104900264230f3ef597d9ea
Joseph Lander <josephclander@gmail.com>
2023-01-08 12:30:53 +0000

    Edit logo position on hero

--- src/Components/Core/Hero.js
+++ src/Components/Core/Hero.js
@@ -5,9 +5,7 @@ const hero = () => {
   return (
     <div className='hero'>
       <div className='hero__container'>
-        <p className='hero__tagline'>
-          <img className='nav__logo' src={logo} alt='svg logo' />
-        </p>
+        <img className='hero__logo' src={logo} alt='svg logo' />
         <div className='hero__message'>
           <p className='hero__message_1'>Find the best tasting,</p>
           <p className='hero__message_2'>hangover free beers.</p>
