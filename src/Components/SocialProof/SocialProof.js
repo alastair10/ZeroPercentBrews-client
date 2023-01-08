a7b5534b0d9a6eecb66b6e9e90a4ce5247c93334
Alastair <alastair.chau@gmail.com>
2023-01-08 22:29:31 +0000

    WIP - SocialProof fetch returning error: 'blocked by CORS policy' when making patch request

--- src/Components/SocialProof/SocialProof.js
+++ src/Components/SocialProof/SocialProof.js
@@ -1,11 +1,10 @@
-import Rating from './Rating';
-import ReviewCard from './ReviewCard';
-import ButtonPrimary from '../Core/ButtonPrimary';
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import Rating from "./Rating";
+import ReviewCard from "./ReviewCard";
+import ButtonPrimary from "../Core/ButtonPrimary";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 const SocialProof = (props) => {
-
   const [error, setError] = useState(undefined);
   const user_id = window.localStorage.getItem("user_id");
   const token = window.localStorage.getItem("token");
@@ -13,76 +12,70 @@ const SocialProof = (props) => {
   const [commentBody, setCommentBody] = useState("");
 
   const handleChange = (e) => {
-    /// handle change to comment body
     setCommentBody(e.target.value);
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // API endpoint ..../api/beers/{id}/comments 
-    
-  // It needs thhe below in the request body:
-  //   {
-  //     "comment": {
-  //         "user_id": {This should be the user_id of the user who is logged in},
-  //         "body": String 
-  //     }
-  // }
-  // `http://localhost:4000/api/beers/${id}/reviews`
-    fetch(`https://zero-percent-brews-api.onrender.com/api/beers/${id}/reviews`, 
-      {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
+    // `http://localhost:4000/api/beers/${id}/reviews`
+    // `https://zero-percent-brews-api.onrender.com/api/beers/${id}/reviews`
+    fetch(`https://zero-percent-brews-api.onrender.com/api/beers/${id}/reviews`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        comments: {
+          user_id: user_id,
+          body: commentBody,
         },
-        body: JSON.stringify({
-          comments: {
-            user_id: user_id,
-            body: commentBody
-          }
-        }),
-      }
-    ).then((response) => {
-      if (response.ok) {
-        navigate(-1);
-      }
-    }).catch(e => {
-      setError(e.message);
-    });
-
-
-  }
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          setCommentBody("");
+          console.log("comment added to DB");
+        }
+      })
+      .catch((e) => {
+        setError(e.message);
+      });
+  };
 
   return (
     <>
-      {props.beerData && (<>
-        <Rating beerData={props.beerData} />
-        
-        <div className='comment_box'>
-          <h2>What's brewin'?</h2>
-          <form>
-          <input
-              type="text"
-              name="comment"
-              value={commentBody}
-              onChange={handleChange}
-            />
-          <ButtonPrimary text={"Submit"} onClick={handleSubmit}/>
-          </form>
-        { error && <p>error</p>}
-        </div>
+      {props.beerData && (
+        <>
+          <Rating beerData={props.beerData} />
 
-        {props.beerData.comments.map((comment) => {
-          return <ReviewCard author={comment.user_id.username} key={comment._id} body={comment.body} />
-        })}
+          <div className="comment_box">
+            <h2>What's brewin'?</h2>
+            <form>
+              <input
+                type="text"
+                name="comment"
+                value={commentBody}
+                onChange={handleChange}
+              />
+              <ButtonPrimary text={"Submit"} onClick={handleSubmit} />
+            </form>
+            {error && <p>error</p>}
+          </div>
 
-      </>)}
-
-
+          {props.beerData.comments.map((comment) => {
+            return (
+              <ReviewCard
+                author={comment.user_id.username}
+                key={comment._id}
+                body={comment.body}
+              />
+            );
+          })}
+        </>
+      )}
     </>
-  )
-
-}
+  );
+};
 
 export default SocialProof;
