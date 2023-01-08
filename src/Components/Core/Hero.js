4b75c5d1b4cf45378c917eb6c5473a25da2cf3f1
Joseph Lander <josephclander@gmail.com>
2023-01-08 12:16:31 +0000

    Swap header and home buttons

--- src/Components/Core/Hero.js
+++ src/Components/Core/Hero.js
@@ -1,19 +1,16 @@
 import './Hero.css';
+import logo from '../../images/primary-logo.svg';
 
 const hero = () => {
   return (
     <div className='hero'>
       <div className='hero__container'>
         <p className='hero__tagline'>
-          <span className='hero__span'>Actually</span> Not That Bad
+          <img className='nav__logo' src={logo} alt='svg logo' />
         </p>
-        <div className="hero__message">
-          <p className='hero__message_1'>
-            Find the best tasting,
-          </p>
-          <p className='hero__message_2'>
-            hangover free beers.
-          </p>
+        <div className='hero__message'>
+          <p className='hero__message_1'>Find the best tasting,</p>
+          <p className='hero__message_2'>hangover free beers.</p>
         </div>
       </div>
     </div>
