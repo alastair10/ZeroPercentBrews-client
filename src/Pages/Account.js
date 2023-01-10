2dc262e1b77098b0a87c97daa742106ec1111708
Alastair <alastair.chau@gmail.com>
2023-01-10 15:35:25 +0000

    Links added to saved list items

--- src/Pages/Account.js
+++ src/Pages/Account.js
@@ -1,7 +1,6 @@
-import styles from "../Components/Beer/BeerCard.module.css";
-
 import Hero from "../Components/Core/Hero";
 import { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 
 const Account = () => {
   const user_id = window.localStorage.getItem("user_id");
@@ -30,16 +29,18 @@ const Account = () => {
             message_2={userData.username}
           />
           <h1 className="beer__title">Account information:</h1>
-          <div class="basic__info__item">
-            <span class="attribute">Username: </span>
+          <div className="basic__info__item">
+            <span className="attribute">Username: </span>
             {userData.username}</div>
-          <div class="basic__info__item">
-            <span class="attribute">Email:</span> {userData.email}
+          <div className="basic__info__item">
+            <span className="attribute">Email:</span> {userData.email}
           </div>
           <h1 className="beer__title">Your saved beers:</h1>
           <div>
             {userData.saved.map((savedBeers) => {
-              return <p>{savedBeers.title}</p>;
+              return (<Link key = {savedBeers.title} to={`/beer/${savedBeers._id}`}><p>{savedBeers.title}</p>
+              </Link>
+              )
             })}
           </div>
         </>
