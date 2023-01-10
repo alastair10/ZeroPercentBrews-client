55764c38ff492280378cbbb2cb1ce5c9754bad25
Tom <tomfyfe85@gmail.com>
2023-01-10 16:09:17 +0000

    password can be changed. displays confirmation massage

--- src/Pages/Account.js
+++ src/Pages/Account.js
@@ -3,17 +3,15 @@ import Hero from "../Components/Core/Hero";
 import { useState } from "react";
 import ButtonPrimary from "../Components/Core/ButtonPrimary";
 
-
 const Account = () => {
   const user_id = window.localStorage.getItem("user_id");
   // const [savedBeers, setSavedBeers] = useState("");
-  const [newPassword, setNewPassword] = useState('');
-  const token = window.localStorage.getItem('token')
+  const [newPassword, setNewPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const token = window.localStorage.getItem("token");
   const handleChange = (e) => {
-    setNewPassword(e.target.value)
-     
-  }
-
+    setNewPassword(e.target.value);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,35 +22,31 @@ const Account = () => {
         method: "PATCH",
         headers: {
           "Content-Type": "application/json",
-           Authorization: `Bearer ${token}` 
+          Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
           newPassword: newPassword,
         }),
       }
     ).then((response) => {
-      console.log(response)
-    })
-   };
-
-
+      if (response.ok) {
+        setMessage("Password Updated");
+      }
+      console.log(response.json());
+    });
+  };
 
   // will need to populate username instead of user_id below
   // pull in BeerContainer to map through saved results
   return (
     <>
       <Hero message_1={"Account details for,"} message_2={user_id} />
-      <div></div>
+      <div>{message}</div>
       <form>
-        <label>
-          Change Password:
-        </label>
-        <input type="password"
-        value={newPassword}
-        onChange={handleChange}/>
-        <ButtonPrimary text={'Submit'} onClick={handleSubmit}/>
+        <label>Change Password:</label>
+        <input type="password" value={newPassword} onChange={handleChange} />
+        <ButtonPrimary text={"Submit"} onClick={handleSubmit} />
       </form>
-      
     </>
   );
 };
