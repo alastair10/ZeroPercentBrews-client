a13422e6b9c67d5191c64112c804394e7e56ec8c
adonl <adnlwly@gmail.com>
2023-01-10 19:38:19 +0000

    add Type dropdown

--- src/Components/Beer/BeerContainer.js
+++ src/Components/Beer/BeerContainer.js
@@ -6,11 +6,11 @@ import ButtonTertiary from '../Core/ButtonTertiary';
 const BeerContainer = ({ beerData, setBeerData }) => {
 
   const getMostLiked = () => {
-      fetch(`http://localhost:4000/api/beers/most-liked`)
-        .then((response) => response.json())
-        .then((data) => {
-          setBeerData(data);
-        });
+    fetch(`http://localhost:4000/api/beers/most-liked`)
+      .then((response) => response.json())
+      .then((data) => {
+        setBeerData(data);
+      });
   };
 
   const getLowestCal = () => {
@@ -19,13 +19,24 @@ const BeerContainer = ({ beerData, setBeerData }) => {
       .then((data) => {
         setBeerData(data);
       });
-};
+  };
 
   return (
     <>
       <div className={styles.refinement_field}>
         <ButtonTertiary text={'Most Liked'} onClick={getMostLiked} />
         <ButtonTertiary text={'Low Cal'} onClick={getLowestCal} />
+        <select
+          value={''}
+          onChange={''}
+          name=""
+          className="discover__dropdown"
+        >
+          <option value="">Type</option>
+          <option value="Ale">Ale</option>
+          <option value="Cider">Cider</option>
+          <option value="Lager">Lager</option>
+        </select>
       </div>
       <div className={styles.beer_container}>
         {beerData.map((beer) => {
