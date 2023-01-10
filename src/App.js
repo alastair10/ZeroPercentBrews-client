9e77fa854a9a8aab8bf4a97a1883deb1ed7470c6
Joseph Lander <josephclander@gmail.com>
2023-01-10 11:36:48 +0000

    WIP authcontext

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
-import BeerListing from "./Pages/BeerListing";
-import Home from "./Pages/Home";
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./Components/Core/Nav";
-import Footer from "./Components/Core/Footer";
-import Register from "./Pages/Register";
-import Login from "./Pages/Login";
-import Account from "./Pages/Account"
+import BeerListing from './Pages/BeerListing';
+import Home from './Pages/Home';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Core/Nav';
+import Footer from './Components/Core/Footer';
+import Register from './Pages/Register';
+import Login from './Pages/Login';
+import Account from './Pages/Account';
+import PrivateRoute from './Auth/PrivateRoute';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(window.localStorage.getItem("token"));
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    window.localStorage.getItem('token')
+  );
 
   return (
     <>
       <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
       <Routes>
-        <Route path="/account" element= {<Account />} />
-        <Route path="/register" element={<Register isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
-        <Route path="/login" element={<Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/beer/:id" element={<BeerListing />} />
+        {/* <Route path="/account" element= {<Account />} /> */}
+        <PrivateRoute path='/account' element={<Account />} />
+        <Route
+          path='/register'
+          element={
+            <Register isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+          }
+        />
+        <Route
+          path='/login'
+          element={
+            <Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+          }
+        />
+        <Route path='/' element={<Home />} />
+        <Route path='/beer/:id' element={<BeerListing />} />
+        <Route
+          path='/register'
+          element={
+            <Register isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+          }
+        />
+        <Route
+          path='/login'
+          element={
+            <Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+          }
+        />
+        <Route path='/' element={<Home />} />
+        <Route path='/beer/:id' element={<BeerListing />} />
       </Routes>
-
       <Footer />
     </>
   );
--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
+import AuthContext from './Auth/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <AuthContext.Provider value={false}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </AuthContext.Provider>
 );
-
