806766c82cf064be59b185744c201c970a1d7979
Alastair <alastair.chau@gmail.com>
2023-01-10 10:17:14 +0000

    Add imports Hero and BeerContainer

---
+++ src/Pages/Account.js
@@ -0,0 +1,14 @@
+import Hero from '../Components/Core/Hero';
+import BeerContainer from '../Components/Beer/BeerContainer';
+const  = (Account) => {
+  
+  
+  
+  return (
+    <>
+      <Hero message_1={'Account details for,'} message_2={'USERNAME_HERE'} />
+    </>
+  );
+}
+ 
+export default Account;
