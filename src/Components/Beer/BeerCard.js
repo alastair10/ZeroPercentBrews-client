d2cdafcbfa230b36fa818468d769433cddc1ee95
Joseph Lander <josephclander@gmail.com>
2023-01-11 14:20:24 +0000

    Align new upvote button

--- src/Components/Beer/BeerCard.js
+++ src/Components/Beer/BeerCard.js
@@ -1,15 +1,16 @@
-import styles from "./BeerCard.module.css";
-import ButtonPrimary from "../Core/ButtonPrimary";
-import ButtonTertiary from "../Core/ButtonTertiary";
-import ButtonSecondary from "../Core/ButtonSecondary";
-import Badge from "../../images/staff_pick.png";
-import { useState } from "react";
-import { useAuth } from "../../Auth/AuthContext";
+import styles from './BeerCard.module.css';
+import ButtonUpVote from '../Core/ButtonUpVote';
+import ButtonPrimary from '../Core/ButtonPrimary';
+import ButtonTertiary from '../Core/ButtonTertiary';
+import ButtonSecondary from '../Core/ButtonSecondary';
+import Badge from '../../images/staff_pick.png';
+import { useState } from 'react';
+import { useAuth } from '../../Auth/AuthContext';
 
 const BeerCard = ({ beerInfo, parent }) => {
   const [error, setError] = useState(undefined);
-  const token = window.localStorage.getItem("token");
-  const user_id = window.localStorage.getItem("user_id");
+  const token = window.localStorage.getItem('token');
+  const user_id = window.localStorage.getItem('user_id');
   const id = beerInfo._id;
   const [kegs, setKegs] = useState(beerInfo.kegs);
   const [isSaved, setIsSaved] = useState(false);
@@ -22,9 +23,9 @@ const BeerCard = ({ beerInfo, parent }) => {
     let response = await fetch(
       `https://zero-percent-brews-api.onrender.com/api/beers/${id}/kegs`,
       {
-        method: "PATCH",
+        method: 'PATCH',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
@@ -46,9 +47,9 @@ const BeerCard = ({ beerInfo, parent }) => {
     fetch(
       `https://zero-percent-brews-api.onrender.com/api/user/${user_id}/saved`,
       {
-        method: "PATCH",
+        method: 'PATCH',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
@@ -66,9 +67,9 @@ const BeerCard = ({ beerInfo, parent }) => {
     fetch(
       `https://zero-percent-brews-api.onrender.com/api/user/${user_id}/saved`,
       {
-        method: "PATCH",
+        method: 'PATCH',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
@@ -86,48 +87,45 @@ const BeerCard = ({ beerInfo, parent }) => {
   return (
     <div className={styles.beer}>
       {beerInfo.staffPick && (
-        <img className={styles.badge} src={Badge} alt="staff pick badge" />
+        <img className={styles.badge} src={Badge} alt='staff pick badge' />
       )}
       <img
         className={styles.beer__image}
         src={beerInfo.image}
-        alt="beer"
-        loading="lazy"
+        alt='beer'
+        loading='lazy'
       />
       <div className={styles.info__container}>
+        <h3 className={styles.beer__title}>{beerInfo.title}</h3>
         <div className={styles.beer__basic__info}>
-          <h3 className={styles.beer__title}>{beerInfo.title}</h3>
-
-          {isLoggedIn && (
-            <div className={styles.social__proof}>
-              <div className={styles.upvote}>
-                <div className={styles.upvote__score}>{kegs}</div>
+          <div className={styles.beer__info}>
+            {isLoggedIn && (
+              <div className={styles.social__proof}>
+                <div className={styles.upvote}>
+                  <div className={styles.upvote__score}>{kegs}</div>
+                </div>
+                <ButtonUpVote handleKegVote={handleKegVote} />
               </div>
-              <button onClick={handleKegVote} className={styles.upvote__button}>
-                ⬆️
-              </button>
+            )}
+            <div className={styles.basic__info__item}>
+              <span className={styles.attribute}>Type:</span> {beerInfo.type}
             </div>
-          )}
-
-          <div className={styles.basic__info__item}>
-            <span className={styles.attribute}>Type:</span> {beerInfo.type}
+            <div className={styles.basic__info__item}>
+              <span className={styles.attribute}>Calories:</span>{' '}
+              {beerInfo.calories} kcal
+            </div>
           </div>
-          <div className={styles.basic__info__item}>
-            <span className={styles.attribute}>Calories:</span>{" "}
-            {beerInfo.calories} kcal
-          </div>
-        </div>
-
-        {parent === "beerContainer" ? (
-          <ButtonPrimary path={`/beer/${beerInfo._id}`} text={"More Info"} />
+        {parent === 'beerContainer' ? (
+          <ButtonPrimary path={`/beer/${beerInfo._id}`} text={'More Info'} />
         ) : (
           isLoggedIn &&
-          (parent === "beerListing" && !isSaved ? (
-            <ButtonSecondary text={"Save"} onClick={handleSave} />
+          (parent === 'beerListing' && !isSaved ? (
+            <ButtonSecondary text={'Save'} onClick={handleSave} />
           ) : (
-            <ButtonTertiary text={"Unsave"} onClick={handleUnsave} />
+            <ButtonTertiary text={'Unsave'} onClick={handleUnsave} />
           ))
         )}
+        </div>
       </div>
     </div>
   );
