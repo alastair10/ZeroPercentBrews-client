f427d80c20a37ed995780327e309b9a1a94db13a
Alastair <alastair.chau@gmail.com>
2023-01-11 10:49:06 +0000

    Raw date now pulling into ReviewCard

--- src/Components/SocialProof/ReviewCard.js
+++ src/Components/SocialProof/ReviewCard.js
@@ -1,11 +1,11 @@
 import './ReviewCard.css';
 
-const ReviewCard = ({ author, body }) => {
+const ReviewCard = ({ author, body, date }) => {
   return (
     <>
       <div className='review__info'>
         <div className='review__header'>
-          <h3 className='review__title'>Comment</h3>
+          <h3 className='review__title'>{date}</h3>
         </div>
         <div className='review__body'>{body}</div>
         <div className='review__author'>{author}</div>
