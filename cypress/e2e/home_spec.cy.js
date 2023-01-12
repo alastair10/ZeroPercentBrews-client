d614667e8d2500a4acdaabc1141876a9dc89fc75
Tom <tomfyfe85@gmail.com>
2023-01-12 14:54:42 +0000

    signup and home buttons work.'More Info' button - WIP

--- cypress/e2e/home_spec.cy.js
+++ cypress/e2e/home_spec.cy.js
@@ -8,7 +8,24 @@ describe("Log In and Sign up buttons send user to relevant pages", () => {
     cy.get(".ButtonTertiary_button__5kBiD").contains("Log In").click();
 
     cy.url().should("include", "/login");
-  });
+  })
 
+  it("takes user to sign up page", () => {
+    cy.get(".ButtonSecondary_button__wgU6f").contains("Sign Up").click();
 
-});
+    cy.url().should("include", "/register");
+  })
+
+  it("takes user to home page", () => {
+    cy.get(".nav__link--logo").click();
+    cy.url("localhost:3000/");
+    })
+
+  it("more info button takes user to  beer listing page ", () => {
+    cy.get(".ButtonPrimary_button__MIZnn").contains("More Info").click();
+    
+    cy.url().should("include", `${/beerInfo._id}`)
+  }
+  )
+
+})
