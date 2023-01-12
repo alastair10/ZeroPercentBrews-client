c0165fab85c92d54495dd647b22e0bd2fd9a2e1b
Alastair <alastair.chau@gmail.com>
2023-01-12 13:57:30 +0000

    Cypress test passing: Can navigate to login page via homepage

--- cypress/e2e/sign_in_spec.cy.js
+++ cypress/e2e/sign_in_spec.cy.js
@@ -1,36 +1,26 @@
-// describe('Signing In', () => {
-//   beforeEach(() => {
-//     cy.visit('https://www.zeropercentbrews.com/')
-//   })
-  
-//   it("navigates to the account page when logged in", () => {
-    
-//     cy.get('.hero__span').should('contain', 'Actually')
-//   })
-  
-
-// })
 describe("Signing in", () => {
-
-  // beforeEach(() => {
-  //   cy.visit('http://localhost:3000')
-  // })
-
-  it("displays an error with invalid password", () => {
+  it("can navigate to the login page via the button on the hompage", () => {
     cy.visit("http://localhost:3000/login");
-    cy.get('input[name=email]').type("alastairtest@example.com");
-    cy.get('input[name=password]').type("alastairtest1");
-    cy.get(".ButtonPrimary_button__MIZnn").contains("Submit").click();
-
-    cy.get(".Access_error_message__qRlAo").should('contain', 'Invalid login credentials');
+    cy.get(".ButtonTertiary_button__5kBiD").contains('Log In');
+    
+    cy.url().should("include", "/login");
   });
 
   it("displays an error with invalid password", () => {
     cy.visit("http://localhost:3000/login");
-    cy.get('input[name=email]').type("alastairtest1@example.com");
-    cy.get('input[name=password]').type("alastairtest");
+    cy.get("input[name=email]").type("alastairtest@example.com");
+    cy.get("input[name=password]").type("alastairtest1");
     cy.get(".ButtonPrimary_button__MIZnn").contains("Submit").click();
 
-    cy.get(".Access_error_message__qRlAo").should('contain', 'Invalid login credentials');
+    cy.get(".Access_error_message__qRlAo").should("contain", "Invalid login credentials");
+  });
+
+  it("displays an error with invalid password", () => {
+    cy.visit("http://localhost:3000/login");
+    cy.get("input[name=email]").type("alastairtest1@example.com");
+    cy.get("input[name=password]").type("alastairtest");
+    cy.get(".ButtonPrimary_button__MIZnn").contains("Submit").click();
+
+    cy.get(".Access_error_message__qRlAo").should("contain", "Invalid login credentials");
   });
 });
